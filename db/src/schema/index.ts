6680c9569f827b20ab66909099e6793bf10da96b
ecq <miguelsegundario2011@gmail.com>
2026-06-29 17:40:27 -0300

    Delete db directory

--- db/src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./users";
-export * from "./api-keys";
-export * from "./managed-apis";
-export * from "./api-endpoints";
-export * from "./plans";
-export * from "./request-logs";
-export * from "./webhooks";
